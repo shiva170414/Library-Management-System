698a42b025d106f7fe7c006208cd98fb173153e7
SHivanand <130499678+shiva170414@users.noreply.github.com>
2024-04-23 00:01:52 +0530

    Add files via upload

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.*;
+
+/**
+ *
+ * @author sdshi
+ */
+public class ConnectionProvider {
+    public static Connection getCon()
+    {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/lms","root","W7301@jqir#");
+            return con;
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            return null;
+        }
+    }
+    
+}
